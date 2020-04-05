51f3f0f9a8adbc85969f2abdfc47cf8166adaa48
jc2548461 <abc2548461@gmail.com>
2020-04-05 22:53:15 +0800

    two empty java file

---
+++ dbload.java
@@ -0,0 +1,15 @@
+
+
+
+public class dbload {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(String s :args){
+			System.out.println(s+"  "+s.length());
+		}
+		
+    }
+    
+}
---
+++ dbquery.java
@@ -0,0 +1,13 @@
+
+
+public class dbquery {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		for(String s:args) {
+			System.out.println(s);
+		}
+		
+	}
+
+}
