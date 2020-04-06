8b5329c627bba52fb77b175dcfc0df65820bf9b4
jc2548461 <abc2548461@gmail.com>
2020-04-06 08:51:07 +0800

    dbload, read csv

--- dbload.java
+++ dbload.java
@@ -1,11 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class dbload {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int pagesize = 4096;
-		String filepath = "";
+		int pageSize = 4096;
+		String readFilePath = "";
+		String writeFilePath = "./data";
 		boolean argsRight = false;
 		
 		for(String s :args){
@@ -17,10 +24,10 @@ public class dbload {
 			try {
 				if(args[0].equals("-p")) {
 					System.out.println("args[0] true");
-					filepath = args[2];
-					pagesize = Integer.parseInt( args[1] );
-					System.out.println("filepath:"+filepath);
-					System.out.println("pagesize:"+pagesize);
+					writeFilePath = args[2];
+					pageSize = Integer.parseInt( args[1] );
+					System.out.println("filepath:"+writeFilePath);
+					System.out.println("pagesize:"+pageSize);
 					argsRight = true;
 				}
 			}catch(Exception e){
@@ -28,8 +35,56 @@ public class dbload {
 			}
 		}
 		
+		argsRight = true;
+		pageSize = 4096;
+		readFilePath = "D:\\File\\Desktop\\java database\\code\\test.csv";
+		
+		
+		if(argsRight)
+		{
+			List<String> data = readCsv(new File(readFilePath));
+			System.out.println("over");
+			
+		}else {
+			System.out.println("args error");
+		}
+		
+		
 		System.out.println("main func over");
 		
-    }
+    }//end of main
+	
+	
+    /**  
+     * read data from csv file  
+     *  
+     * @param file : file path  
+     * @return a String list contains data in file
+     */   
+	public static List<String> readCsv(File file){
+		List<String> data=new ArrayList<String>();
+        
+        BufferedReader br=null;
+        try { 
+            br = new BufferedReader(new FileReader(file));
+            String line = ""; 
+            while ((line = br.readLine()) != null) {
+            	data.add(line);
+            }
+        }catch (Exception e) {
+        }finally{
+            if(br!=null){
+                try {
+                    br.close();
+                    br=null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+ 
+        return data;
+    }//end of readCsv
+	
     
-}
+}//end of class dbloadgit
