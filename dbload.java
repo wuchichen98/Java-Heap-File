d3f5c6c537fabd78245d6659bd37796a4a0d930a
jc2548461 <abc2548461@gmail.com>
2020-04-06 09:44:06 +0800

    dbload,write a page at once

--- dbload.java
+++ dbload.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class dbloadgit {
+public class dbload {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -46,6 +49,7 @@ public class dbloadgit {
 			int[] typeList = {1,1,1,1,2,2,2,1,1,1,2,2,2,2,2,2,3,3,4};
 			List<String> data = readCsv(new File(readFilePath));
 			int length_byte = getLineLength(typeList);
+			writeHeadFile(writeFilePath,data,pageSize,typeList,length_byte);
 			System.out.println("over");
 			
 		}else {
@@ -120,4 +124,86 @@ public class dbloadgit {
 	}//end of getLineLength
 	
     
+	
+    /** write the Head File
+     * @param filePath:file path
+     * @param data: a String list
+     * @param pageSize: page size
+     * @param typeList: a int array,the data type and bytes count of line data
+     * @param length_byte: the count of bytes
+     * @return void
+     */
+    public static void writeHeadFile(String filePath,List<String>  data,int pageSize,int[] typeList,int length_byte){    
+       File file = new File(filePath);
+       try {
+           if(file.exists()){
+               file.createNewFile();
+               System.out.println("create New File: "+filePath);
+           }
+           int pagei = 0,pagecount=0;;
+           byte[] pageContent = new byte[pageSize];
+           Arrays.fill(pageContent, (byte) 0);
+           for(int i =1;i<data.size();i++) {
+        	   //System.out.println(i);
+        	   byte[] content = lineStrToBytes(lineStrToList(data.get(i)),typeList,length_byte);
+        	   
+        	   if(length_byte + pagei < pageSize) {
+        		   System.arraycopy(content, 0, pageContent, pagei, length_byte);
+        		   pagei += length_byte;
+        	   }
+        	   else {
+        		   WriteByteToFile(filePath,pageContent,true);
+        		   Arrays.fill(pageContent, (byte) 0);
+        		   pagei = 0;
+        		   System.arraycopy(content, 0, pageContent, pagei, length_byte);
+        		   pagei += length_byte;
+        		   pagecount++;
+        		   //System.out.println("page count:"+pagecount);
+        	   }
+        	   
+        	   
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+    }
+    
+    /** write one page bytes array to Head File,
+     * @param path: file path
+     * @param content: one page bytes array
+     * @param Appendable: Appendable
+     * @return void
+     */
+    public static void WriteByteToFile(String path, byte[] content, boolean Appendable) throws IOException {
+        FileOutputStream fos = new FileOutputStream(path,Appendable);
+        fos.write(content);
+        fos.close();
+    }//end of WriteByteToFile
+    
+    /** change one line str to a string list
+     * @param lineStr: one line str
+     * @return a string list
+     */
+	public static List<String> lineStrToList(String lineStr){
+
+    	List<String> lineStrList = new ArrayList<String>();
+
+    	return lineStrList;
+    
+	}//end of lineStrToList
+	
+    /** change a string list to bytes array
+     * @param lineStrList: a string list
+     * @param typeList: data type of every data value
+     * @param length_byte: length of bytes array
+     * @return bytes array
+     */	
+    public static byte[] lineStrToBytes(List<String> lineStrList,int[] typeList,int length_byte) throws UnsupportedEncodingException {
+    	byte[] lineBytes = new byte[length_byte];
+
+        return lineBytes;
+    }//end of lineStrToBytes
+  
+	
+	
 }//end of class dbloadgit
